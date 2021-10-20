75c287ed3553abf4f795e1a5c8141435332d48a7
amitranjan771 <92822187+amitranjan771@users.noreply.github.com>
2021-10-20 03:51:22 +0100

    restructure
    restructure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rivrtest",
-    version="1.0.1",
+    version="1.0.2",
     author="Amit Ranjan",
     author_email="amitranjanora@gmail.com",
     url="https://2qlu4yb1d4.execute-api.eu-central-1.amazonaws.com/dev/rivr",
