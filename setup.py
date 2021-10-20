8d84cb0ff73926fa6b83045d3f3b7086537e8cd2
amitranjan771 <92822187+amitranjan771@users.noreply.github.com>
2021-10-20 03:38:13 +0100

    test packaging
    test packaging

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="amitranjanora@gmail.com",
     url="https://2qlu4yb1d4.execute-api.eu-central-1.amazonaws.com/dev/rivr",
     description="A sample python app for rivr",
-    packages=find_packages(),
     package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=[],
 )
