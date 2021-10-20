060d1d597083bee596b225df558071ea0c0b8aba
amitranjan771 <92822187+amitranjan771@users.noreply.github.com>
2021-10-20 02:00:05 +0100

    Update setup.py
    actual url in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0.0",
     author="Amit Ranjan",
     author_email="amitranjanora@gmail.com",
-    url="dummyurl",
+    url="https://2qlu4yb1d4.execute-api.eu-central-1.amazonaws.com/dev/rivr",
     description="A sample python app",
     packages=find_packages(),
     install_requires=[],
