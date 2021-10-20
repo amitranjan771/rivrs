e31c844ac67c79bcaa29225190780a4958768919
amitranjan771 <92822187+amitranjan771@users.noreply.github.com>
2021-10-20 04:39:42 +0100

    testing
    testing

---
+++ handler.py
@@ -0,0 +1,11 @@
+import json
+
+def rivr(event, context):
+    body = {
+        "message": "This is a test for cicd pipeline from github to aws lambda function"
+    }
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+    return response
